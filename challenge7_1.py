be9f7ab9e5fba600726a8d128f2c666f21d029fb
xue99999 <1090374167@qq.com>
2018-07-03 00:20:41 +0800

    pass

--- challenge7_1.py
+++ challenge7_1.py
@@ -1,33 +1,31 @@
 import pandas as pd
 
 def data_clean():
+    global countries
     data = pd.read_excel('ClimateChange.xlsx', sheetname='Data')
-
-    data[data['Series code'] == 'EN.ATM.CO2E.KT'].set_index('Country code')
-
-    data.drop(labels=['Country code', 'Series name', 'Series code', 'SCALE', 'Decimals'], axis=1, inplace=True)
-
-    data.replace({'..': pd.np.NaN}, inplace=True)
-
-    data.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1)
-
-    data = data.dropna(how='all', inplace=True)
-
-    data['Sum emissions'] = data.sum(axis=1)
-
-    data = data['Sum emissions']
-
-    countries = pd.read_excel('ClimateChange.xlsx', sheetname='Country')
-
-    countries = countries.set_index('Country code', inplace=True)
-
-    countries.drop(labels=['Capital city', 'Region', 'Lending category'], axis=1, inplace=True)
-    
-    return pd.concat([data, countries], axis=1)
+    data = data[data['Series code'] == 'EN.ATM.CO2E.KT'].set_index('Country code')
+    data = data.iloc[:, 5:].replace({'..': pd.np.nan})
+    data = data.fillna(method='ffill', axis=1).fillna(method='bfill', axis=1)
+    data.dropna(how='all', inplace=True)
+    countries = pd.read_excel('ClimateChange.xlsx', sheetname='Country').set_index('Country code')
+    return pd.concat([data.sum(axis=1), countries['Income group']], axis=1)
 
 def co2():
-    data = data_clean()
+    df = data_clean()
+    df_sum = df.groupby('Income group').sum()
+    df_sum.columns = ['Sum emissions']
+    df['haha'] = countries['Country name']
+    df_max = df.sort_values(0, ascending=False).groupby('Income group').head(1).set_index('Income group')
 
-    data = 
+    df_max.columns = ['Highest emissions', 'Highest emission country']
+    
+    df_min = df.sort_values(0).groupby('Income group').head(1).set_index('Income group')
+    df_min.columns = ['Lowest emissions', 'Lowest emission country']
+
+    result = pd.concat([df_sum, df_max.sort_index(axis=1), df_min.sort_index(axis=1)], axis=1)
+#    return df_max
+    return result
 
 
+if __name__ == '__main__':
+    print(co2())
